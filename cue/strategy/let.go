dbc730998a61ebfd02a3ec0602dd6c13c6ead3c7
Markus Bointner <bointner.markus9@gmail.com>
2024-07-14 12:06:13 +0200

    extract common code

--- cue/strategy/let.go
+++ cue/strategy/let.go
@@ -2,7 +2,6 @@ package strategy
 
 import (
 	"cuelang.org/go/cue/ast"
-	"cuelang.org/go/cue/ast/astutil"
 	"github.com/mandoway/seru/reduction"
 )
 
@@ -13,51 +12,10 @@ type LetReduction struct {
 
 // Apply returns variants of the input where exactly one let statement was transformed
 func (r LetReduction) Apply(input *ast.File) []*ast.File {
-
-	var transformed []*ast.File
-	var seenClausesInThisRun, modifiedClauses int
-
-	modifyNextLetClause := func(cursor astutil.Cursor) bool {
-		// Filter applicable nodes
-		letClause, ok := cursor.Node().(*ast.LetClause)
-		if !ok {
-			return true
+	return applyTransformationToEveryApplicableStatement[*ast.LetClause](input, func(node *ast.LetClause) ast.Node {
+		return &ast.Field{
+			Label: ast.NewIdent(node.Ident.Name),
+			Value: node.Expr,
 		}
-		// Filter already applied nodes
-		seenClausesInThisRun++
-
-		alreadyModifiedStatementInThisRun := modifiedClauses > len(transformed)
-		statementWasModifiedInPreviousRun := seenClausesInThisRun-1 < modifiedClauses
-		if alreadyModifiedStatementInThisRun || statementWasModifiedInPreviousRun {
-			return false
-		}
-
-		fieldFromClause := &ast.Field{
-			Label: ast.NewIdent(letClause.Ident.Name),
-			Value: letClause.Expr,
-		}
-
-		cursor.Replace(fieldFromClause)
-
-		modifiedClauses++
-		return false
-	}
-
-	modifiedClauses = 0
-	previous := -1
-	for {
-		seenClausesInThisRun = 0
-		previous = modifiedClauses
-
-		workingCopy := *input
-		transformedCode := astutil.Apply(&workingCopy, modifyNextLetClause, nil).(*ast.File)
-
-		if modifiedClauses == previous {
-			break
-		}
-
-		transformed = append(transformed, transformedCode)
-	}
-
-	return transformed
+	})
 }
