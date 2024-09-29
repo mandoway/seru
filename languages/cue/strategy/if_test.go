c83e195ae68517638fe818dbe2e76707893567a7
Markus Bointner <bointner.markus9@gmail.com>
2024-09-29 18:18:21 +0200

    uncomment tests

--- languages/cue/strategy/if_test.go
+++ languages/cue/strategy/if_test.go
@@ -7,144 +7,144 @@ import (
 
 func TestIfReduction(t *testing.T) {
 	instances := []test.ReductionInstance{
-		//{
-		//	Title: "not applicable",
-		//	Given: `
-		//	foo: 4
-		//	bar: foo
-		//	baz: {
-		//		a: foo
-		//	}
-		//	`,
-		//	Expected: []string{},
-		//},
-		//{
-		//	Title: "Creates tautology and removes clause",
-		//	Given: `
-		//	foo: 3
-		//	if foo > 2 {
-		//		bar: 2
-		//	}
-		//	`,
-		//	Expected: []string{
-		//		`
-		//		foo: 3
-		//		if foo == foo {
-		//			bar: 2
-		//		}
-		//		`,
-		//		`
-		//		foo: 3, {
-		//			bar: 2
-		//		}
-		//		`,
-		//	},
-		//},
-		//{
-		//	Title: "Creates contradiction and removes clause&struct",
-		//	Given: `
-		//	foo: 3
-		//	if foo < 2 {
-		//		bar: 2
-		//	}
-		//	`,
-		//	Expected: []string{
-		//		`
-		//		foo: 3
-		//		if foo != foo {
-		//			bar: 2
-		//		}
-		//		`,
-		//		`
-		//		foo: 3
-		//		`,
-		//	},
-		//},
-		//{
-		//	Title: "Applies to selectors too",
-		//	Given: `
-		//	foo: {
-		//		bar: 3
-		//	}
-		//	if foo.bar < 2 {
-		//		baz: 2
-		//	}
-		//	`,
-		//	Expected: []string{
-		//		`
-		//		foo: {
-		//			bar: 3
-		//		}
-		//		if foo.bar != foo.bar {
-		//			baz: 2
-		//		}
-		//		`,
-		//		`
-		//		foo: {
-		//			bar: 3
-		//		}
-		//		`,
-		//	},
-		//},
-		//{
-		//	Title: "scope",
-		//	Given: `
-		//	foo: 3
-		//	if foo < 3 {
-		//		bar: 1
-		//	}
-		//	a: {
-		//		foo: 2
-		//		if foo < 3 {
-		//			bar: 2
-		//		}
-		//	}
-		//	`,
-		//	Expected: []string{
-		//		`
-		//		foo: 3
-		//		if foo != foo {
-		//			bar: 1
-		//		}
-		//		a: {
-		//			foo: 2
-		//			if foo < 3 {
-		//				bar: 2
-		//			}
-		//		}
-		//		`, `
-		//		foo: 3
-		//		if foo < 3 {
-		//			bar: 1
-		//		}
-		//		a: {
-		//			foo: 2
-		//			if foo == foo {
-		//				bar: 2
-		//			}
-		//		}
-		//		`, `
-		//		foo: 3
-		//		a: {
-		//			foo: 2
-		//			if foo < 3 {
-		//				bar: 2
-		//			}
-		//		}
-		//		`,
-		//		`
-		//		foo: 3
-		//		if foo < 3 {
-		//			bar: 1
-		//		}
-		//		a: {
-		//			foo: 2, {
-		//				bar: 2
-		//			}
-		//		}
-		//		`,
-		//	},
-		//},
+		{
+			Title: "not applicable",
+			Given: `
+			foo: 4
+			bar: foo
+			baz: {
+				a: foo
+			}
+			`,
+			Expected: []string{},
+		},
+		{
+			Title: "Creates tautology and removes clause",
+			Given: `
+			foo: 3
+			if foo > 2 {
+				bar: 2
+			}
+			`,
+			Expected: []string{
+				`
+				foo: 3
+				if foo == foo {
+					bar: 2
+				}
+				`,
+				`
+				foo: 3, {
+					bar: 2
+				}
+				`,
+			},
+		},
+		{
+			Title: "Creates contradiction and removes clause&struct",
+			Given: `
+			foo: 3
+			if foo < 2 {
+				bar: 2
+			}
+			`,
+			Expected: []string{
+				`
+				foo: 3
+				if foo != foo {
+					bar: 2
+				}
+				`,
+				`
+				foo: 3
+				`,
+			},
+		},
+		{
+			Title: "Applies to selectors too",
+			Given: `
+			foo: {
+				bar: 3
+			}
+			if foo.bar < 2 {
+				baz: 2
+			}
+			`,
+			Expected: []string{
+				`
+				foo: {
+					bar: 3
+				}
+				if foo.bar != foo.bar {
+					baz: 2
+				}
+				`,
+				`
+				foo: {
+					bar: 3
+				}
+				`,
+			},
+		},
+		{
+			Title: "scope",
+			Given: `
+			foo: 3
+			if foo < 3 {
+				bar: 1
+			}
+			a: {
+				foo: 2
+				if foo < 3 {
+					bar: 2
+				}
+			}
+			`,
+			Expected: []string{
+				`
+				foo: 3
+				if foo != foo {
+					bar: 1
+				}
+				a: {
+					foo: 2
+					if foo < 3 {
+						bar: 2
+					}
+				}
+				`, `
+				foo: 3
+				if foo < 3 {
+					bar: 1
+				}
+				a: {
+					foo: 2
+					if foo == foo {
+						bar: 2
+					}
+				}
+				`, `
+				foo: 3
+				a: {
+					foo: 2
+					if foo < 3 {
+						bar: 2
+					}
+				}
+				`,
+				`
+				foo: 3
+				if foo < 3 {
+					bar: 1
+				}
+				a: {
+					foo: 2, {
+						bar: 2
+					}
+				}
+				`,
+			},
+		},
 		{
 			Title: "no evaluation",
 			Given: `
